f32998deced6af5ec220ede7eed4505f0fe17e9d
Vladimir <kyourselfer@gmail.com>
2018-05-13 21:14:38 +0300

    added emainling signals

--- lesson5_bash/watchdog/main.sh
+++ lesson5_bash/watchdog/main.sh
@@ -18,21 +18,25 @@ function fcheck {
 		#
 		# VALUES
 		cPID=/tmp/$(basename $0).pid
+		lPID=/tmp/$(basename $0).lock
+		email='kyourselfer@ya.ru'
 		#
 		# catching Psignals
 		trap 'rm -f $cPID; echo "$cPID has removed"' EXIT
-		trap ' echo "$0 is closing ..." ; exit 3 ' 15
+		trap 'echo "$0 is closing ..."; exit 3' 15
+		trap -- 1 3 17 18
 		#
 		# find pidfile
 		if [ -e $cPID ]
 		then
-			    echo "script can not start untill $cPID is existed, it has just deleted and everething is OK"
+			    echo -e "Process can not start. It's existed already."
 			        exit 2
 			fi
 			#
-			# create pidfile
+			# create pidfile and lockfile
 			echo $$ > $cPID
-			echo "$cPID has created"
+			flock -w $lPID ...........................sh
+			echo "$cPID has created and blocked"
 			#
 			proc=$1
 			#
@@ -43,24 +47,10 @@ function fcheck {
 				        fcheck
 					    if [ $? = 1 ]
 						        then
-								    echo "script can not start untill $cPID is existed, it has just deleted and everething is OK"
-								        exit 2
-								fi
-								#
-								# create pidfile
-								echo $$ > $cPID
-								echo "$cPID has created"
-								#
-								proc=$1
-								#
-								fcheck
-								while [ $? = 0 ]
-								do
-									    sleep 5
-									        fcheck
-										    if [ $? = 1 ]
-											        then
-													        `systemctl start $proc`
-														    fi
-													    done
+								        clogs=`journalctl -n 5 -u $proc`
+									        cinfo=`uptime | awk -Fe: '{print "avg:" $2}'; date; echo $clogs`
+										        `echo $cinfo | mail -s "$proc was restarted" $email`
+											        `systemctl start $proc`
+												    fi
+											    done
 
